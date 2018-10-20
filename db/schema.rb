c353fc736fa2ad54a816e02520ca17ae14f0d62d
Deseree Caraballo <decaraballo@cps.edu>
2018-10-20 16:14:49 -0500

    added authors

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_20_190945) do
+ActiveRecord::Schema.define(version: 2018_10_20_193909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "country"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
---
+++ spec/factories/authors.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :author do
+    first_name { "MyString" }
+    last_name { "MyString" }
+    country { "MyString" }
+    age { 1 }
+  end
+end

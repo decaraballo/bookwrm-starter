abeb4cb0e6a17a890c65d0a4373edbbc6e2ccc19
Deseree Caraballo <decaraballo@cps.edu>
2018-10-21 14:11:59 -0500

    reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
+	has_many :reviews
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         def full_name
+   return 'Anonymous' unless (first_name && last_name)      	
+first_name + " " + last_name
+end
+
+def initials
+	return 'AN' unless (first_name && last_name)
+	first_name.try(:first) + '.' + last_name.try(:first) + '.'
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :reviews
 	root to: 'books#index'
-  resources :books
+  resources :books do
+  	 resources :reviews
+  	end
   resources :authors
   devise_for :users
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
